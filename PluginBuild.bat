2bc2cb0993521fe4d2a99dac279b7f49de26f36e
Dexied <33525453+Dexied@users.noreply.github.com>
2026-01-25 03:35:46 +0500

    570-002

--- PluginBuild.bat
+++ PluginBuild.bat
@@ -1,1 +1,5 @@
-B:\UnrealEngineSource\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="B:\Projects\XProjectileDemo\Plugins\XProjectile\XProjectile.uplugin" -Package="B:\Projects\XProjectileDemo\Builds\XProjectile" -Rocket -2019
+@echo off
+B:\UnrealEngineSource\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="B:\Projects\XProjectileDemo\Plugins\XProjectile\XProjectile.uplugin" -Package="B:\Projects\XProjectileDemo\Builds\XProjectile" -Rocket -2019 > "B:\Projects\XProjectileDemo\BuildLog.txt" 2>&1
+echo Build completed. Check BuildLog.txt for details.
+pause
+
